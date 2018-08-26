717d03431560ecf5122593b747b8027aad23390a
Vitor José <vitorjsls30@gmail.com>
2018-08-26 01:29:58 -0300

    blg-01 - Adding PostsIndex and PostsNew Component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return(
+            <div>
+                Posts new
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
